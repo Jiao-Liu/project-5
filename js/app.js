1c7404fc96a8b57ba6dba84d363628eff3b4eacf
Jiao-Liu <jiaoliu0228@gmail.com>
2021-06-24 12:50:55 -0500

    first time submit

--- js/app.js
+++ js/app.js
@@ -1,2 +1,22 @@
-baguetteBox.run('.gallery');
-mySearchFilter.run(".search");
+baguetteBox.run(".grid");
+
+
+document.getElementById('search').addEventListener("keyup", myFunction)
+
+function myFunction(){
+ 
+    var text = document.getElementById("search").value.toLowerCase();
+    var imglist = document.getElementsByTagName('img');
+   
+
+    for ( let i = 0; i< imglist.length; i++ ) {
+       var dataCaption = document.getElementsByTagName('a')[i].getAttribute('data-caption')
+    
+        if (dataCaption.toLowerCase().indexOf(text) > -1) {
+            imglist[i].style.display = '';
+        } else {
+            imglist[i].style.display = 'none';
+        }
+    }
+}
+
